7a38824bcd7a7104809bfdf43ad6dcc3b89dbd6e
dawniris <deversz@renewfund.com>
2015-12-18 14:31:59 -0800

    hearkening back to the tagging days of yore

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,5 @@
 class Recipe < ActiveRecord::Base
   validates :name, presence: true
+  has_many :tags, through: :recipe_tags
+  has_many :recipe_tags
 end
--- spec/features/create_new_recipe_spec.rb
+++ spec/features/create_new_recipe_spec.rb
@@ -1,12 +1,14 @@
 RSpec.feature 'create_new_recipe' do
 
+let!(:tag) { FactoryGirl.create(:tag) }
+
   scenario 'valid information' do
     visit root_path
     click_on 'new recipe'
 
     fill_in 'Name', :with => 'pants'
     ## haven't bothered implementing type yet; suck it
-    # select 'type'
+    check tag.name
     click_on 'Create Recipe'
 
     expect(page.body).to have_link 'pants'
